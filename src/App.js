c0327335345bad843009b076a5557f500836203b
Kamal Bhera <kamalbhera96@gmail.com>
2022-12-23 22:39:47 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
+import React, { useState } from 'react'
 import logo from './logo.svg';
 import './App.css';
+import KeyPad from './components/KeyPad';
+import Result from './components/Result';
 
 function App() {
+  const [result, setResult] = useState(0);
+
+  const hendelClick = button => {
+    if (button === "=") {
+      calculate()
+    } else if (button === "C") {
+      reset()
+    } else if (button === "CE") {
+      backspace()
+    } else {
+      setResult(result + button)
+    }
+  };
+
+  const calculate = () => {
+    var checkResult = ''
+    if (result.includes('--')) {
+      checkResult = result.replace('--', '+')
+    } else {
+      checkResult = result
+    }
+    try {
+      // eslint-disable-next-line
+      setResult((eval(checkResult)))
+    } catch (e) {
+      console.log(e);
+      setResult("Error", e)
+    }
+  };
+
+  const reset = () => {
+    setResult("")
+  };
+  const backspace = () => {
+    setResult(result.slice(0, -1))
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>Calculator</code> useing React Hooks.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div className="calculator-body">
+        <Result result={result} />
+        <KeyPad onClick={hendelClick} />
+      </div>
       </header>
     </div>
   );
---
+++ src/components/Result.js
@@ -0,0 +1,9 @@
+import React from 'react';
+const Result = ({result}) => {
+    return (
+        <div className="result">
+            <p>{result}</p>
+        </div>
+    );
+}
+export default Result;
